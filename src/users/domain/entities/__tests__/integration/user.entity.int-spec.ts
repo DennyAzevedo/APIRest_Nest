0612addf7af61ccf6da1fbedaff812b2d5854a4e
Denny Azevedo <denny.filho@animaeducacao.com.br>
2024-06-20 14:34:14 -0300

    :globe_with_meridians: Integration Tests - E-mail validation :test_tube:

--- src/users/domain/entities/__tests__/integration/user.entity.int-spec.ts
+++ src/users/domain/entities/__tests__/integration/user.entity.int-spec.ts
@@ -23,10 +23,45 @@ describe('UserEntity integration tests', () => {
 			}
 			expect(() => new UserEntity(valid)).toThrow(EntityValidationError)
 		})
+		it('Should throw an error when creating a user with invalid name - no string', () => {
+			const valid: UserProps = {
+				... props,
+				name: 10 as any,
+			}
+			expect(() => new UserEntity(valid)).toThrow(EntityValidationError)
+		})
 		it('Should throw an error when creating a user with invalid name - large', () => {
 			const valid: UserProps = {
 				... props,
 				name: 'a'.repeat(256) as any,
+			}
+			expect(() => new UserEntity(valid)).toThrow(EntityValidationError)
+		})
+		it('Should throw an error when creating a user with invalid email - null', () => {
+			const valid: UserProps = {
+				... props,
+				email: null as any,
+			}
+			expect(() => new UserEntity(valid)).toThrow(EntityValidationError)
+		})
+		it('Should throw an error when creating a user with invalid email - empty', () => {
+			const valid: UserProps = {
+				... props,
+				email: '' as any,
+			}
+			expect(() => new UserEntity(valid)).toThrow(EntityValidationError)
+		})
+		it('Should throw an error when creating a user with invalid email - no string', () => {
+			const valid: UserProps = {
+				... props,
+				email: 10 as any,
+			}
+			expect(() => new UserEntity(valid)).toThrow(EntityValidationError)
+		})
+		it('Should throw an error when creating a user with invalid email - large', () => {
+			const valid: UserProps = {
+				... props,
+				email: 'a'.repeat(256) as any,
 			}
 			expect(() => new UserEntity(valid)).toThrow(EntityValidationError)
 		})
